7d8f1325741538dbebc81bebd41d8ddf44cfc984
Barbour-Condini <46252587+Barbour-Condini@users.noreply.github.com>
2023-12-31 10:58:21 +0000

    Update place3dDice()

--- settings.js
+++ settings.js
@@ -49,27 +49,41 @@ const randomNumber = (min, max) => {
 
 
 const place3dDice = () => {
-for (let j=0; j<2; j++) {
-for (let i = 0; i < 11; i++) {
-    setTimeout(() => {
-        const dice = document.createElement('img');
-        dice.src = `assets/images/3d/${i}.png`;
-        dice.id = `img${i}`;
-        dice.class = '3dImg'
-        document.body.appendChild(dice);
+    for (let i = 0; i < 11; i++) {
+        setTimeout(() => {
+            const dice = document.createElement('img');
+            dice.src = `assets/images/3d/${i}.png`;
+            dice.id = `img${i}`;
+            dice.class = '3dImg'
+            document.body.appendChild(dice);
 
-        const fadeTime = randomNumber(1, 5);
-        dice.style.animation = `fadeIn ${fadeTime}s`;
+            const fadeTime = randomNumber(1, 5);
+            dice.style.animation = `fadeIn ${fadeTime}s`;
 
-        dice.style.position = 'fixed';
-        dice.style.top = `${randomNumber(30, 98)}%`;
-        dice.style.left = `${randomNumber(0, 98)}%`;
-        dice.style.transform = `rotate(${randomNumber(0, 360)}deg)`;
-        dice.style.width = `${randomNumber(50, 300)}px`;
-        dice.style.zIndex = '-1';
-    }, (randomNumber(0, 2000)));
-}
-}
+            dice.style.position = 'fixed';
+
+            const positions = [
+                { top: [25, 95], left: [5, 20] },
+                { top: [85, 95], left: [25, 75] },
+                { top: [25, 95], left: [80, 95] },
+            ]
+            const index = randomNumber(0, 3)
+
+            const top1 = positions[index].top[0];
+            const top2 = positions[index].top[1];
+            dice.style.top = `${randomNumber(top1, top2)}%`;
+
+            const left1 = positions[index].left[0];
+            const left2 = positions[index].left[1];
+            dice.style.left = `${randomNumber(left1, left2)}%`;
+            // dice.style.top = `${randomNumber(30, 98)}%`;
+            // dice.style.left = `${randomNumber(0, 98)}%`;
+
+            dice.style.transform = `rotate(${randomNumber(0, 360)}deg)`;
+            dice.style.width = `${randomNumber(50, 300)}px`;
+            dice.style.zIndex = '-1';
+        }, (randomNumber(0, 2000)));
+    }
 }
 
 place3dDice();
